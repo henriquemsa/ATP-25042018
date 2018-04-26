e53fddc0cab5c41a7e556ee5bcffadd33ace496a
Alef Henrique <ahmsantos@sga.pucminas.br>
2018-04-26 15:37:31 -0300

    Exercício 02
    Faça um método para realizar a multiplicação e a divisão de 2 números inteiros. Retorne e imprima os dois resultados.

--- Program.cs
+++ Program.cs
@@ -1,43 +1,45 @@
 ﻿/*
- * Faça um método que calcule e imprima a multiplicação de 2 valores inteiros recebidos.
+ * Faça um método para realizar a multiplicação e a divisão de 2 números inteiros. Retorne e imprima os dois resultados.
  */
+ 
+ using System;
 
-using System;
-
-namespace Exercicio_01
+namespace Exercicio_02
 {
     class Program
     {
-        // Calcula e retorna o valor do produto de 'a' por 'b'
-        static int Multiplica (int a, int b)
+        // Calcula e retorna o valor do produto de 'a' por 'b' e o valor da divisão de 'a' por 'b'
+        static double MultiplicaDivide(int a, int b, out double divisao)
         {
-            int resultado;
+            double resultado;
             resultado = a * b;
+            divisao = a / b;
             return resultado;
         }
 
         static void Main(string[] args)
         {
             // Declaração de variáveis
-            int a, b, resultado;
+            int a, b;
+            double resultado, divisao;
 
+            // Recebendo valores do usuário
             Console.Clear(); // Limpa tela
             Console.WriteLine(); // Pula linha
-
-            // Recebendo os valores do usuário
             Console.Write("Entre com o valor de 'a': ");
             a = int.Parse(Console.ReadLine());
             Console.Write("Entre com o valor de 'b': ");
             b = int.Parse(Console.ReadLine());
 
-            // Chamando o método multiplica
-            resultado = Multiplica(a, b);
+            // Chamando o método MultiplicaDivide
+            resultado = MultiplicaDivide(a, b, out divisao);
 
             // Imprimindo o resultado
             Console.Clear(); // Limpa tela
             Console.WriteLine(); // Pula linha
             Console.Write("O valor da multiplicação de {0} por {1} é: {2}", a, b, resultado);
-            Console.ReadKey();
+            Console.Write("O valor da divisão de {0} por {1} é: {2}", a, b, divisao);
+            Console.ReadKey();  
         }
     }
 }
